3307bbf081545b7365d282371dfa85d47fa33ae1
leogtzr <leogutierrezramirez@gmail.com>
2021-03-21 10:53:24 -0700

    add test to check for next tokens

--- src/main/java/com/monkeyj/lexer/Lexer.java
+++ src/main/java/com/monkeyj/lexer/Lexer.java
@@ -1,14 +1,47 @@
 package com.monkeyj.lexer;
 
+import com.monkeyj.token.TokenConstants;
+import com.monkeyj.token.TokenType;
+
+import static com.monkeyj.token.TokenConstants.*;
+
 public class Lexer {
 
     private String input;
     private int position;               // current position in input (points to the current char).
     private int readPosition;           // current reading position in input (after current char).
-    private byte ch;                    // current char under examination.
+    private char ch;                    // current char under examination.
 
     public Lexer(final String input) {
         this.input = input;
+        this.readChar();
+    }
+
+    private void readChar() {
+        if (this.readPosition >= this.input.length()) {
+            this.ch = 0;
+        } else {
+            this.ch = this.input.charAt(this.readPosition);
+        }
+        this.readPosition++;
+    }
+
+    public TokenType nextToken() {
+        final var tok = switch (this.ch) {
+            case '=' -> new TokenType(ASSIGN, this.ch + "");
+            case ';' -> new TokenType(SEMICOLON, this.ch + "");
+            case '(' -> new TokenType(LPAREN, this.ch + "");
+            case ')' -> new TokenType(RPAREN, this.ch + "");
+            case ',' -> new TokenType(COMMA, this.ch + "");
+            case '+' -> new TokenType(PLUS, this.ch + "");
+            case '{' -> new TokenType(LBRACE, this.ch + "");
+            case '}' -> new TokenType(RBRACE, this.ch + "");
+            case Character.MIN_VALUE -> new TokenType(EOF, "");
+            default -> throw new IllegalArgumentException(String.format("Not expected -> [%s]", this.ch));
+        };
+
+        this.readChar();
+        return tok;
     }
 
 }
--- src/test/java/com/monkeyj/lexer/LexerTest.java
+++ src/test/java/com/monkeyj/lexer/LexerTest.java
@@ -1,7 +1,40 @@
 package com.monkeyj.lexer;
 
+import com.monkeyj.token.TokenConstants;
 import org.junit.jupiter.api.Test;
 
+import static com.monkeyj.token.TokenConstants.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LexerTest {
+
+    @Test
+    public void shouldRetrieveNextToken() {
+        final String INPUT = "=+(){},;";
+
+        record test(String expectedType, String expectedLiteral) {}
+
+        final test []tests = {
+            new test(ASSIGN, "="),
+            new test(PLUS, "+"),
+            new test(LPAREN, "("),
+            new test(RPAREN, ")"),
+            new test(LBRACE, "{"),
+            new test(RBRACE, "}"),
+            new test(COMMA, ","),
+            new test(SEMICOLON, ";"),
+            new test(EOF, ""),
+        };
+
+        final var lex = new Lexer(INPUT);
+
+        for (final test test : tests) {
+            var tok = lex.nextToken();
+            assertEquals(tok.type(), test.expectedType);
+            assertEquals(tok.literal(), test.expectedLiteral);
+        }
+
+    }
 
 }
