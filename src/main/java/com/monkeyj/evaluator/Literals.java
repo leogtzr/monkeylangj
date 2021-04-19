c37a111514ae450c116fde7a3a6915126f435fbb
leogtzr <leogutierrezramirez@gmail.com>
2021-04-18 20:39:24 -0600

    add null literal

--- src/main/java/com/monkeyj/evaluator/Literals.java
+++ src/main/java/com/monkeyj/evaluator/Literals.java
@@ -1,6 +1,7 @@
 package com.monkeyj.evaluator;
 
 import com.monkeyj.object.Bool;
+import com.monkeyj.object.Null;
 
 public final class Literals {
 
@@ -8,5 +9,6 @@ public final class Literals {
 
     public static final Bool TRUE = new Bool(true);
     public static final Bool FALSE = new Bool(false);
+    public static final Null NULL = new Null();
 
 }
